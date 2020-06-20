4aaf9cd0ed111d34b51df09544ba04a193e932d6
Shreeansh Gupta <sarojvarshney20@gmail.com>
2020-06-20 21:46:48 +0530

    updated dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 const router = express.Router();
+const Story = require("../models/Story");
 
 // @route -> GET /
 // @desc  -> Login/landing page
@@ -14,10 +15,17 @@ router.get("/", ensureGuest, (req, res) => {
 // @route -> GET /dashboard
 // @desc  -> get the user dashboard
 
-router.get("/dashboard", ensureAuth, (req, res) => {
-  res.render("dashboard", {
-    name: req.user.firstName,
-  });
+router.get("/dashboard", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean();
+    res.render("dashboard", {
+      name: req.user.firstName,
+      stories,
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.render("errors/404");
+  }
 });
 
 module.exports = router;
